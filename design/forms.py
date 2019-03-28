0655291d094819196a35a05579e77eb5a7339eec
laxit-patel <patellaxit8@gmail.com>
2019-03-28 15:54:07 +0530

    Design Form Remodeled

--- design/forms.py
+++ design/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.forms import TextInput
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout, Submit, HTML, Field, Button
+from crispy_forms.layout import Layout, Submit, HTML, Field, Button, Div
 from crispy_forms.bootstrap import PrependedText, Div
 from modules.key_generator import key_generator
 from design.models import Design
@@ -31,14 +31,59 @@ class New_Design_Form(forms.ModelForm):
 
 
         self.helper.layout = Layout(
-            Field('Design_Id', css_class='w-100 form-control text-dark', wrapper_class='form-group', ),
-            Field('Design_Dimension', css_class='w-100 form-control text-dark', wrapper_class='form-group', ),
-            Field('Design_Connection', css_class='w-100 form-control text-dark', wrapper_class='form-group', ),
-            Field('Design_Fpi', css_class='w-100 form-control text-dark', wrapper_class='form-group', ),
-            Field('Design_Tube', css_class='w-100 form-control text-dark', wrapper_class='form-group', ),
-            Field('Design_Hairpin', css_class='w-100 form-control text-dark', wrapper_class='form-group', ),
-            Field('Design_Drawing', css_class='w-100 form-control text-dark ', wrapper_class='form-group', ),
-            Field('Design_Revision', css_class='w-100 form-control text-dark', wrapper_class='form-group', ),
-            Field('Design_Origin', css_class='w-100 form-control text-dark', wrapper_class='form-group', ),
-            Submit('submit', 'Add & Exit', css_class='btn btn-block btn-outline-dark'),
+            Div(
+                Div(
+                    Field('Design_Id', css_class='w-100 form-control text-dark', wrapper_class='form-group', ),
+                    css_class="col-md-4"
+                ),
+                Div(
+                    Field('Design_Dimension', css_class='w-100 form-control text-dark', wrapper_class='form-group', ),
+                    css_class="col-md-8"
+                ),
+                css_class="row"
+            ),
+            Div(
+                Div(
+                    Field('Design_Connection', css_class='w-100 form-control text-dark', wrapper_class='form-group', ),
+                    css_class="col-md-4"
+                ),
+                Div(
+                    Field('Design_Fpi', css_class='w-100 form-control text-dark', wrapper_class='form-group', ),
+                    css_class="col-md-4"
+                ),
+                Div(
+                    Field('Design_Tube', css_class='w-100 form-control text-dark', wrapper_class='form-group', ),
+                    css_class="col-md-4"
+                ),
+                css_class="row"
+            ),
+            Div(
+                Div(
+                    Field('Design_Hairpin', css_class='w-100 form-control text-dark', wrapper_class='form-group', ),
+                    css_class="col-md-4"
+                ),
+                Div(
+                    Field('Design_Revision', css_class='w-100 form-control text-dark', wrapper_class='form-group', ),
+                    css_class="col-md-4"
+                ),
+                Div(
+                    Field('Design_Drawing', css_class='w-100 form-control text-dark ', wrapper_class='form-group', ),
+                    css_class="col-md-4"
+                ),
+                css_class="row"
+            ),
+            Div(
+                Div(
+                    Field('Design_Origin', css_class='w-100 form-control text-dark custom-select', wrapper_class='form-group', ),
+                    css_class="col-md-8"
+                ),
+                Div(
+                    HTML("<label for='submit'>&nbsp</label>"),
+                    Submit('submit', 'Add', css_class='btn btn-block btn-outline-success text-white'),
+                    css_class="col-md-4"
+                ),
+                css_class="row"
+            ),
+
+
         )
